372a4982fd25a50a1dfe8d8761baac681d4476f2
Alessandro M <38500291+AleTM1@users.noreply.github.com>
2021-02-28 16:39:27 +0100

    0.3

--- main.py
+++ main.py
@@ -40,39 +40,58 @@ def selection(lenght_array):
     return population_indexes
 
 
-def crossover(best, dim, num_act):
+def crossover(best, dim, num_act, best_lenghts):
+    def heuristic_choice():
+        total = sum(best_lenghts)
+        v = random.randint(1, total)
+        for j in range(len(best_lenghts)):
+            if v - best_lenghts[j] <= 0:
+                return best[j]
+            v -= best_lenghts[j]
+
     population = best
     n = len(best)
     while len(population) < dim:
-        new_car = copy.deepcopy(random.choice(best))
-        new_car.actions_generator(int(new_car.tick * 0.6), num_act)
+        new_car = copy.deepcopy(heuristic_choice())
+        new_car.actions_generator(int(new_car.tick * 0.4), num_act)
         population.append(new_car)
     for i in range(n):
         population[i].actions_generator(int(population[i].tick * 0.9), num_act)
     return population
 
 
+def termination(lenght_array, waypoints_array):
+    for i in range(len(lenght_array)):
+        if lenght_array[i] > 700:
+            for p in range(int(len(waypoints_array[i])/2), len(waypoints_array[i])):
+                if np.linalg.norm((waypoints_array[i][p]) - np.array([0, 0])) < 4:
+                    return True, i, p
+    return False, -1, -1
+
+
 def main_loop(actions_num, dim):
-    population = [Car(actions_num - 1, inner_poly, outer_poly) for _ in range(dim)]
-    epoch = 0
     while True:
-        lenght_array, waypoints_array = fitness_calculation(population)
-        if max(lenght_array) > 700:
-            index = 0
-            for w in waypoints_array:
-                if np.linalg.norm(np.array((0, 0)) - w[-1]) < 5:
-                    display([waypoints_array[index]], line_in, line_out, str(epoch) + "_COMPLETE")
-                    return 0
-                index += 1
-        selection_arr = selection(lenght_array)
-        best = [population[i] for i in selection_arr]
-        if epoch % 15 == 0:
-            display(waypoints_array, line_in, line_out, str(epoch))
-        population = copy.deepcopy(crossover(best, dim, actions_num - 1))
-        epoch += 1
+        population = [Car(actions_num - 1, inner_poly, outer_poly) for _ in range(dim)]
+        epoch = 0
+        while epoch < 180:
+            lenght_array, waypoints_array = fitness_calculation(population)
+            if max(lenght_array) > 700:
+                t, index, point = termination(lenght_array, waypoints_array)
+                if t:
+                    display([waypoints_array[index][:point]], line_in, line_out, str(epoch) + '_COMPLETE')
+                    return population[index]
+            selection_arr = selection(lenght_array)
+            best = [population[i] for i in selection_arr]
+            best_lenghts = [lenght_array[i] for i in selection_arr]
+            if epoch % 30 == 0:
+                display(waypoints_array, line_in, line_out, str(epoch))
+            population = copy.deepcopy(crossover(best, dim, actions_num - 1, best_lenghts))
+            epoch += 1
 
 
 time = 100
 population_dim = 20
-main_loop(time, population_dim)
+car = main_loop(time, population_dim)
+print(car.actions)
+print(car.tick)
 print("COMPLETE")
