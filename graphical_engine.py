0324d80de9bbfe31493219fcef68f681ffcca885
Alessandro M <38500291+AleTM1@users.noreply.github.com>
2021-03-11 03:03:18 +0100

    sectors 0.1

--- graphical_engine.py
+++ graphical_engine.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.cm as mcm
 
+from car import Car
+
 
 def display_running(waypoints_array, line_in, line_out, sectors, epoch):
     fig = plt.figure(1, figsize=(24, 16))
@@ -35,7 +37,7 @@ def display_running(waypoints_array, line_in, line_out, sectors, epoch):
     #fig.savefig('saves/' + epoch + '.png')
 
 
-def display_ending(solution, line_in, line_out, epoch):
+def display_ending(solution, line_in, line_out, sectors, epoch):
     fig = plt.figure(1, figsize=(24, 16))
     ax = fig.add_subplot(111, facecolor='black')
 
@@ -43,6 +45,10 @@ def display_ending(solution, line_in, line_out, epoch):
         def plot_border(ob, col):
             x, y = ob.xy
             ax.plot(x, y, color=col, alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
+
+        def plot_sectors(segments_coords, col):
+            for segment in segments_coords:
+                ax.plot(segment[:2], segment[2:4], color=col, alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
 
         def plot_raceline():
             x = []
@@ -53,11 +59,12 @@ def display_ending(solution, line_in, line_out, epoch):
                     x.append(waypoints[i, 0])
                     y.append(waypoints[i, 1])
                     colors.append(car.actions[i, 0])
-            col = np.subtract(1, np.subtract(np.array(colors), min(colors)) / max(colors))
+            col = np.subtract(1, np.subtract(np.array(colors), Car.min_spd) / Car.max_spd)
             ax.scatter(x, y, marker='.', s=50, linewidths=4, c=col, cmap=mcm.RdYlBu, zorder=4)
 
-        plot_border(line_in, "white")
-        plot_border(line_out, "white")
+        plot_border(line_in, "cyan")
+        plot_border(line_out, "cyan")
+        plot_sectors(sectors, "white")
         plot_raceline()
 
     print_frame()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from track_loader import load_track
 from car import Car
 import numpy as np
 import shapely.geometry.polygon
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, LineString, Point
 import random
 import copy
 
@@ -18,7 +18,7 @@ outer_poly = Polygon(line_out)
 
 
 def fitness_calculation(car_array):
-    PENALIZATION_PARAM = 8
+    PENALIZATION_PARAM = 6
     waypoints_array = []
     lenght_array = []
     fitness_array = []
@@ -66,40 +66,41 @@ def crossover(best, dim, num_act, best_fitness):
     return population
 
 
-def termination(lenght_array, waypoints_array, end_point):
+def termination(lenght_array, waypoints_array, sector_line):
+    line = LineString([Point(sector_line[0], sector_line[2]), Point(sector_line[1], sector_line[3])])
     for i in range(len(lenght_array)):
-        if lenght_array[i] > 300:
+        if lenght_array[i] > 100:
             for p in range(int(len(waypoints_array[i])/2), len(waypoints_array[i])):
-                if np.linalg.norm((waypoints_array[i][p]) - np.array(end_point)) < 4:
+                if Point(waypoints_array[i][p]).distance(line) < 10:
+                    print(Point(waypoints_array[i][p]).distance(line))
                     return True, i, p
     return False, -1, -1
 
 
-def main_loop(actions_num, dim, sp_array, sa_array):
-    populations = []
-    for k in range(len(sp_array)):
-        population = [Car(actions_num - 1, inner_poly, outer_poly, sp_array[k], sa_array[k]) for _ in range(dim)]
-        populations.append(population)
+def main_loop(actions_num, dim, sp, sa):
+    def init_population(start, ang):
+        return [Car(actions_num - 1, inner_poly, outer_poly, start, ang) for _ in range(dim)]
 
+    population = init_population(sp, sa[0])
     solution = []
     j = 1
     epoch = 0
-    for population in populations:
-        if j == len(populations):
-            j = 0
+    for sector in sectors:
         while True:
             lenght_array, fitness_array, waypoints_array = fitness_calculation(population)
-            if max(lenght_array) > 300:
-                t, index, point = termination(lenght_array, waypoints_array, sp_array[j])
+            if False and epoch % 4 == 0:
+                display_running(waypoints_array, line_in, line_out, sectors, str(epoch))
+            if max(lenght_array) > 100:
+                t, index, point = termination(lenght_array, waypoints_array, sector)
                 if t:
                     best_car = population[index]
                     solution.append([copy.deepcopy(waypoints_array[index][:point]), best_car])
+                    if j < len(sectors):
+                        population = init_population(waypoints_array[index][point], sa[j])
                     break
             selection_arr = selection(fitness_array)
             best = [population[i] for i in selection_arr]
             best_fitness = [fitness_array[i] for i in selection_arr]
-            if epoch % 30 == 0:
-                display_running(waypoints_array, line_in, line_out, sectors, str(epoch))
             population = copy.deepcopy(crossover(best, dim, actions_num - 1, best_fitness))
             epoch += 1
         j += 1
@@ -108,8 +109,8 @@ def main_loop(actions_num, dim, sp_array, sa_array):
 
 time = 100
 population_dim = 50
-starting_point = [[0, 10], [0, -210]]
-starting_angle = [0, 180]
+starting_point = [0, 0]
+starting_angle = [0, -90, 180, 90]
 sol, ep = main_loop(time, population_dim, starting_point, starting_angle)
-display_ending(sol, line_in, line_out, ep)
+display_ending(sol, line_in, line_out, sectors, ep)
 print("COMPLETED in " + str(ep) + " generations")
