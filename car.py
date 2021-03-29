e66850ffcdea9f525d482722afc20a2598216bf7
Alessandro M <38500291+AleTM1@users.noreply.github.com>
2021-03-29 15:54:19 +0200

    refactoring 4

--- car.py
+++ car.py
@@ -5,30 +5,9 @@ from shapely.geometry import Point
 import copy
 
 
-def rndclosestspeed(spd, min_spd, max_spd):
-    a = -3
-    b = +3
-    if spd < min_spd:
-        a = - (spd - min_spd - 3)
-    elif spd > max_spd:
-        b = max_spd + 3 - spd
-    n_spd = random.random()*(b - a) + a
-    return n_spd
-
-
-def rndclosestangle(ang, min_ang, max_ang):
-    a = -2
-    b = +2
-    if ang < min_ang:
-        a = min_ang - 2 - ang
-    elif ang > max_ang:
-        b = max_ang + 2 - ang
-    n_ang = random.random()*(b - a) + a
-    return n_ang
-
-
 class Car:
 
+    # global parameter of Car
     max_spd = 18
     min_spd = 4
     max_ang = 35
@@ -40,25 +19,19 @@ class Car:
         self.starting_pos = starting_point
         self.pos = starting_point
         self.starting_ang = starting_angle
-        self.starting_speed = Car.min_spd
         self.angle = starting_angle
         self.tick = 0
         self.abs_actions = []
         self.rel_actions = []
-        self.actions_generator(0, num_act)
+        self.actions_generator(num_act)  # Creating a new chromosome
         
-    def actions_generator(self, start, num_act):
-        if start == 0:
-            spd = copy.deepcopy(self.starting_speed)
-            ang = 0
-            rel_actions = []
-        else:
-            spd = self.abs_actions[start, 0]
-            ang = self.abs_actions[start, 1]
-            rel_actions = list(self.rel_actions[:start])
-        for i in range(start, num_act):
-            delta_spd = rndclosestspeed(spd, self.min_spd, self.max_spd)
-            delta_ang = rndclosestangle(ang, self.min_ang, self.max_ang)
+    def actions_generator(self, num_act):
+        spd = Car.min_spd
+        ang = 0
+        rel_actions = []
+        for i in range(num_act):
+            delta_spd = self.rndclosestspeed(spd)
+            delta_ang = self.rndclosestangle(ang)
             spd += delta_spd
             ang += delta_ang
             rel_actions.append([delta_spd, delta_ang])
@@ -71,8 +44,8 @@ class Car:
         spd = self.abs_actions[start - 1, 0]
         ang = self.abs_actions[start - 1, 1]
         for i in range(start, end):
-            delta_spd = rndclosestspeed(spd, self.min_spd, self.max_spd)
-            delta_ang = rndclosestangle(ang, self.min_ang, self.max_ang)
+            delta_spd = self.rndclosestspeed(spd)
+            delta_ang = self.rndclosestangle(ang)
             spd += delta_spd
             ang += delta_ang
             self.rel_actions[i][0] = copy.deepcopy(delta_spd)
@@ -80,7 +53,7 @@ class Car:
         self.update_abs_actions()
 
     def update_abs_actions(self):
-        abs_actions = [[self.starting_speed, 0]]
+        abs_actions = [[Car.min_spd, 0]]
         for i in range(0, len(self.rel_actions)):
             abs_act0 = abs_actions[i][0] + self.rel_actions[i][0]
             abs_act1 = abs_actions[i][1] + self.rel_actions[i][1]
@@ -105,3 +78,23 @@ class Car:
         self.angle += action[1]
         self.pos[0] += action[0] * cos(radians(self.angle))
         self.pos[1] += action[0] * sin(radians(self.angle))
+
+    def rndclosestspeed(self, spd):
+        a = -3
+        b = +3
+        if spd < self.min_spd:
+            a = - (spd - self.min_spd - 3)
+        elif spd > self.max_spd:
+            b = self.max_spd + 3 - spd
+        n_spd = random.random()*(b - a) + a
+        return n_spd
+
+    def rndclosestangle(self, ang):
+        a = -2
+        b = +2
+        if ang < self.min_ang:
+            a = self.min_ang - 2 - ang
+        elif ang > self.max_ang:
+            b = self.max_ang + 2 - ang
+        n_ang = random.random()*(b - a) + a
+        return n_ang
